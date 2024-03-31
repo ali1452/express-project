d7da6b7104fcd347ae4bd22b31d7d2bb5b360e40
ali1452 <aliabbas.mkting@gmail.com>
2024-03-31 17:26:44 +0500

    fix

--- controller/productController.js
+++ controller/productController.js
@@ -5,13 +5,11 @@ const Products = require('../modals/productsModal')
 
 const getAllProducts= asyncHandler(async (req, res, next) => {
     const reqQuery = req.query
-        // console.log('get request successfully',reqQuery.product_id)
         if(Object.keys(reqQuery).length >0){
             if(Object.keys(reqQuery).indexOf('product_id:')){
-                console.log('jiyo')
                 const product = await Products.find({product_id:reqQuery.product_id})
                 res.status(200).json(product)
-            }
+              }
             
         }else{
             const products = await Products.find()
